43b8b43331ce3524278b3936f24a5ab526925595
Valeriia Kulish <valeriya0384@gmail.com>
2024-05-24 00:06:34 +0300

    Done1

---
+++ 4.2Guess_the_result.js
@@ -0,0 +1,59 @@
+"use strict"
+
+// let result = 'number' + 3 + 3;
+// console.log(result);
+
+// let result2 = null + 3;
+// console.log(result2);
+
+// let result3 = 5 && "qwerty";
+// console.log(result3);
+
+// let result4 = +'40' + +'2' + "hillel";
+// console.log(result4);
+
+// let result5 = '10' - 5 === 6;
+// console.log(result5);
+
+// let result6 = true + false;
+// console.log(result6);
+
+// let result7 = '4px' - 3;
+// console.log(result7);
+
+// let result8 = '4' - 3;
+// console.log(result8);
+
+// let result9 = '6' + 3 ** 0;
+// console.log(result9);
+
+// let result10 = 12 / '6';
+// console.log(result10);
+
+// let result11 = '10' + (5 === 6);
+// console.log(result11);
+
+//  let result12 = null == '';
+// console.log(result12);
+
+// let result13 = 3 ** (9 / 3);
+// console.log(result13);
+
+// let result14 = !!'false' == !!'true';
+// console.log(result14);
+
+// let result15 = 0 || '0' && 1;
+// console.log(result15);
+
+// let result16 = (+null == false) < 1;
+// console.log(result16);
+
+// let result17 = false && true || true;
+// console.log(result17);
+
+// let result18 = false && (false || true);
+// console.log(result18);
+
+let result19 = (+null == false) < 1 ** 5;
+console.log(result19);
+
